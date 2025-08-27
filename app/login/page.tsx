50de5f58671161ec3883dd664bc7cd1c5d59ca23
Wonmin Cho <0108257@gmail.com>
2025-08-27 14:36:10 +0900

    login 페이지 추가

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+    return (
+        <div>
+            로그인 페이지입니다
+            <div>
+            http://localhost:3000/login 으로 접속할 수 있습니다
+            </div>
+        </div>
+    )
+}
