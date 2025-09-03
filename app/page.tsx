f181093c7d615ef1e82603bb54e3b4d4355688ff
Wonmin Cho <0108257@gmail.com>
2025-09-03 10:46:09 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,21 @@ import { useRouter } from "next/navigation"
 export default function Home() {
   const router = useRouter()
 
+  // const move = (path: string) => router.push(path)
+  function move (path: string) {
+    router.push(path)
+  }
+
   return (
     <div>
       <div> Home </div>
       <div className="p-8 space-y-2">
         <div className="p-2 bg-red-200"
-          onClick={() => router.push('/login')}
+          onClick={() => move('/login')}
         > 로그인으로 이동 </div>
 
         <div className="p-2 bg-blue-200"
-          onClick={() => router.push('/register')}
+        onClick={() => move('/register')}
         > 회원가입으로 이동 </div>
 
         <Link href={"/login"}>
